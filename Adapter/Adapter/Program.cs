8333d501ec1f872347e8cf61a741220a81ad55d0
singhya <ysingh@usc.edu>
2019-11-20 00:13:30 -0800

    Commiting adapter pattern

---
+++ Adapter/Adapter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Adapter
+{
+    public interface IJson
+    {
+        string GetJson();
+    }
+
+    class XML
+    {
+        public string GetXML()
+        {
+            return "This returns an XML string";
+        }
+    }
+
+    class XMLToJson : IJson
+    {
+        private readonly XML _xml;
+
+        public XMLToJson(XML xml)
+        {
+            this._xml = xml;
+        }
+
+        public string GetJson()
+        {
+            return "This converts xml string to json";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XML xml = new XML();
+            IJson json = new XMLToJson(xml);
+
+            Console.WriteLine("XML is incompatible with client");
+            Console.WriteLine("But with XMLToJson, XML can be converted to JSON so that it can be passed in acceptable format to client ");
+
+            Console.WriteLine(json.GetJson());
+        }
+    }
+}
